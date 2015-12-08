1d3c456df35ca68873ddc4e1f4b8c8502d8f3eb7
User <user@Users-MacBook-Pro-7.local>
2015-12-08 08:49:04 +0000

    app homework

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+
+def welcome
+end
+
+def search
+end
+
+def info
+end
+
+end
---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,7 @@
+class InfoController < ApplicationController
+
+  def fixtures
+  end
+
+
+end
